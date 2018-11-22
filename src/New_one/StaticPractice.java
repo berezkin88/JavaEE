920e4fc5ba7b23735752d7e9d8e2c187833075a0
Alexander <berezkin05@gmail.com>
2018-11-22 20:36:01 +0200

    static test

---
+++ src/New_one/StaticPractice.java
@@ -0,0 +1,15 @@
+package New_one;
+
+public class StaticPractice {
+
+    public static void main(String[] args) {
+
+        System.out.println("inside main()");
+
+    }
+
+    static {
+
+        System.out.println("inside static");
+    }
+}
